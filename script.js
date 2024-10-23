351a9b93d9b0d7142306760bc80d1edefe0f932b
Daiury Sousa <141973824+Daiury@users.noreply.github.com>
2024-10-22 22:37:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const cantadas = [
     "Você deve ser um Wi-Fi, porque eu estou sentindo uma conexão.",
     "Se beleza fosse tempo, você seria a eternidade.",
@@ -5,7 +6,10 @@ const cantadas = [
     "Você é como uma luz, ilumina meu caminho.",
     "Se você fosse um sonho, eu nunca queria acordar.",
     "Posso seguir você? Porque meus pais sempre disseram para eu seguir meus sonhos.",
-    "Seu sorriso deve ser um efeito especial, porque ele ilumina meu dia."
+    "Seu sorriso deve ser um efeito especial, porque ele ilumina meu dia.",
+    "Você é mais linda que os gráficos do red dead redemption 2",
+    "Seu beijo é mais gostoso do que chupar picolé na praia",
+    "Ter você na minha vida é mais especial do que todos os meus planos de felicidade, porque com você eles são realidade"
 ];
 
 document.getElementById("gerar").addEventListener("click", () => {
